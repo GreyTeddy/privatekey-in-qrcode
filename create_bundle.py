27b75e939cf908fc3c363fce1a66bcc2fddd1513
GreyTeddy <dukadennis7@gmail.com>
2019-05-13 20:27:23 +0100

    put actions into functions

--- create_bundle.py
+++ create_bundle.py
@@ -6,36 +6,46 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from pyqrcode import QRCode
 
 # create private and public key
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=512,
-    backend=default_backend()
-)
-print("-Created private key")
-public_key = private_key.public_key()
-print("-Created public key")
+def create_private_public_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=512,
+        backend=default_backend()
+    )
+    print("-Created private key")
+    public_key = private_key.public_key()
+    print("-Created public key")
+    return private_key,public_key
 
 # stringify the private and public key
-pem_private = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.PKCS8,
-    encryption_algorithm=serialization.NoEncryption()
-)
-print("-Stringified public key")
-pem_public = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
-print("-Stringified public key")
+def stringify_private_and_public_keys(private_key,public_key):
+    pem_private = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    print("-Stringified public key")
+    pem_public = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    print("-Stringified public key")
+    return pem_private,pem_public
 
 #create public key file
-with open('public_key.pem', 'wb') as f:
-    f.write(pem_public)
-print("-Created public key file")
+def create_private_public_files(pem_public,pem_private):
+    with open('public_key.pem', 'wb') as f:
+        f.write(pem_public)
+    print("-Created public key file")
 
-#create private key svg
-qr = QRCode(pem_private)
-qr.svg("private_key.svg", scale=3)
-print("-Created private key svg")
+    #create private key svg
+    qr = QRCode(pem_private)
+    qr.svg("private_key.svg", scale=3)
+    print("-Created private key svg")
 
-print("-Done")
+
+if __name__ == "__main__":
+    private_key,public_key =  create_private_public_keys()
+    pem_private,pem_public = stringify_private_and_public_keys(private_key,public_key)
+    create_private_public_files(pem_private,pem_public)
+    print("-Done")
